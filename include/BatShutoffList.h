ba78e963c1dc51d97b7020b8be02c5616e4eda26
Joscha Wagner <joscha@arvernus.info>
2022-09-19 08:58:15 +0200

    Fist Prototyp

--- include/BatShutoffList.h
+++ include/BatShutoffList.h
@@ -71,8 +71,8 @@
     BatShutOffActor(Status_FinalSO, Final,      false,      0,      false) \
     BatShutOffActor(Powersafe,      Final,      false,      0,      false) \
     BatShutOffActor(PSU_Gate,       Final,      true,       0,      false) \
-    BatShutOffActor(SSR_Gate,       Final,      true,       0,      false) \
+    BatShutOffActor(SSR_Switch,     Final,      true,       0,      false) \
     BatShutOffActor(Status_OCP,     Ocp,        false,      0,      false) \
-    BatShutOffActor(SSR_Gate,       Ovp,        true,       1000,    true) \
-    BatShutOffActor(SSR_Gate,       Lvp,        true,       1000,    true)
+    BatShutOffActor(SSR_Switch_Ovp, Ovp,        true,       1000,    true) \
+    BatShutOffActor(SSR_Switch_Lvp, Lvp,        true,       1000,    true)
 // clang-format on
---
+++ include/SSRSwitcherList.h
@@ -0,0 +1,26 @@
+/**
+ * @file SSRSwitcherSignalTest.h
+ * @author Joscha Wagner (joscha@nebensound.com)
+ * @brief defines the Signals and limits used
+ *
+ * Copyright (c) Nebensound GmbH 2022
+ *
+ */
+
+/**
+ * @brief list of all SSRs
+ *
+ * @param Name name of the SSR
+ */
+// clang-format off
+//                Name      SignalIdActor   SignalIdSwitch  inverted
+#define SSRSwitcherIdList \
+    SSRSwitcherId(SSR1,     SSR_Gate,       SSR_Switch,     false)
+// clang-format on
+
+// clang-format off
+//                    SSRSwitcherId SignalIdSwitch  ActiveTime  HoldTime    Shunt   CompareType MaxOff  MinOff  MaxOn   MinOn
+#define SSRSwitcherSwitchList \
+    SSRSwitcherSwitch(SSR1,         SSR_Switch_Ovp, 10000,      2000,       Shunt,  MinCompare, -1,     -1,     -0.01,  -0.01) \
+    SSRSwitcherSwitch(SSR1,         SSR_Switch_Lvp, 10000,      2000,       Shunt,  MaxCompare, 1,      1,      0.01,   0.01)
+// clang-format on
