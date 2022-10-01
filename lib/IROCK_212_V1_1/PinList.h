0c78058801b844b1ed3389729d404ddfdef831ed
Andreas Wagner <andreas.wagner@fsa.valantic.com>
2022-10-01 12:40:44 +0200

    Running Ver. with Hardcoded Field I for VEDirect

--- lib/IROCK_212_V1_1/PinList.h
+++ lib/IROCK_212_V1_1/PinList.h
@@ -64,6 +64,6 @@
     Pin(PinV_GND,           PC1,        AnalogInputPin,             1.0,    0.0,    false,  false,  "Virtual Ground") \
     Pin(PinGate,            PC13,       DigitalOutputPin,           1.0,    0.0,    false,  false,  "Gate Driver") \
     Pin(PinGatePSU,         PC14,       DigitalOutputPin,           1.0,    0.0,    false,  false,  "Gate PSU Driver") \
-    Pin(PinVE_Current,      VE_CURRENT, VEDirectPin,                1.0,    0.0,    false,  false,  "Current from VE Shunt") \
-    Pin(PinVE_Current_S,    VE_CURRENT, VEDirectPin,                1.0,    0.0,    false,  false,  "Current from VE Shunt smoothed")
+    Pin(PinVE_Current,      NoPin,      VEDirectPin,                1.0,    0.0,    false,  false,  "Current from VE Shunt") \
+    Pin(PinVE_Current_S,    NoPin,      VEDirectPin,                1.0,    0.0,    false,  false,  "Current from VE Shunt smoothed")
 // clang-format on
