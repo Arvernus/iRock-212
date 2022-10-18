e191ff1d9232bed5f77b391f1ff16a022719403b
Joscha Wagner <joscha@arvernus.info>
2022-10-18 08:13:51 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,16 @@
 #include "main.h"
 #include <Arduino.h>
 #include <TaskManager.h>
-#include <MappingIO.h>
-#include <CLI.h>
-#include <BatShutoff.h>
-#include <Balancer.h>
-#include <SSRSwitcher.h>
 
 void blink()
 {
-  unsigned int time = 1000;
-  if (Signals::GetDigitalValue(Status_FinalSO))
-    time = 100;
-  Signals::SetDigitalValue(LED3, !Signals::GetDigitalValue(LED3));
-  taskManager.scheduleOnce(time, blink);
+  digitalWrite(!digitalRead(PinLED1));
 }
 
 void setup()
 {
-  Cli::setup(115200, true, true, true, true);
-  Signals::Init();
-  // ModBus::setup();
-  blink();
-  String greet;
-  greet = "### Welcome to iRock ###\nYou are running, iRock OS ";
-#define stringer(s) #s
-#define str(s) stringer(s)
-  greet = greet + str(SW_VERSION);
-  greet = greet + " on your iRock 212 ";
-  greet = greet + str(HW_VERSION);
-#undef str
-#undef stringer
-  greet = greet + " in Mapping-Mode ";
-  greet = greet + Mapping::ActualMap();
-  Cli::start(greet);
-  taskManager.yieldForMicros(5 * 1000 * 1000);
-  SSRSwitcher::setup(500);
-  BatShutOff::setup(1000);
-  Balancer::setup(10000, 3, 2000);
+  pinMode(PinLED1, OUTPUT);
+  taskManager.scheduleFixedRate(1000, blink);
 }
 
 void loop()
