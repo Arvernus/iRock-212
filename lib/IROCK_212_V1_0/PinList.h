1c2c50d8f1047aac011de8cac976de3e4eb8f0d2
Joscha Wagner <joscha@arvernus.info>
2022-12-23 16:50:16 +0100

    deleted pis that are no longer in use

--- lib/IROCK_212_V1_0/PinList.h
+++ lib/IROCK_212_V1_0/PinList.h
@@ -63,5 +63,5 @@
     Pin(PinV_GND,           PC1,        AnalogInputPin,             1.0,    0.0,    false,  false,  "Virtual Ground") \
     Pin(PinGate1,           PC13,       DigitalOutputPin,           1.0,    0.0,    false,  false,  "Gate Driver (1)") \
     Pin(PinGate2,           PC14,       DigitalInputPin,            1.0,    0.0,    false,  false,  "Gate Sens (2)") \
-    Pin(PinVE_Current,      NoPin,       VEDirectPin,                1.0,    0.0,    false,  false,  "Current from VE Shunt")
+    Pin(PinVE_Current,      NoPin,      VEDirectPin,                1.0,    0.0,    false,  false,  "Current from VE Shunt")
 // clang-format on
