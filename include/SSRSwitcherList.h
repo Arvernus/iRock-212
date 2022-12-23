8f1fd4694c17cf06f7cc2d7fba35f32eba9635cc
Joscha Wagner <joscha@arvernus.info>
2022-12-23 13:36:47 +0100

    Update SSRSwitcherList.h

--- include/SSRSwitcherList.h
+++ include/SSRSwitcherList.h
@@ -11,6 +11,9 @@
  * @brief list of all SSRs
  *
  * @param Name name of the SSR
+ * @param SignalIdActor Signal that should be switched
+ * @param SignalIdSwitch Signal (virtual digital input) that sores the current value
+ * @param inverted is the SignalIdSwitch inverted?
  */
 // clang-format off
 //                Name      SignalIdActor   SignalIdSwitch
@@ -18,6 +21,21 @@
     SSRSwitcherId(SSR1,     SSR_Gate,       SSR_Switch)
 // clang-format on
 
+/**
+ * @brief list of all signals (switches) that will switch any given SSR INFO: two states at one time are not yet defined
+ *
+ * @param SSRSwitcherId SSR the Switch belongs to
+ * @param SignalIdSwitch input signal
+ * @param ActiveTime time the SSR stays active when status is testing
+ * @param HoldTime time the SSR stays active when status is waiting
+ * @param Shunt condition that needs to be met to
+ * @param CompareType only MinCompare and MaxCompare should be used
+ * @param MinMaxOff depending on CompareType the minimum or maximum value the Shunt can have to stay in status waiting
+ * @param ReleaseOff depending on CompareType the minimum or maximum value the Shunt can have to end status waiting
+ * @param MinMaxOn depending on CompareType the minimum or maximum value the Shunt can have to stay in status testing
+ * @param ReleaseOn depending on CompareType the minimum or maximum value the Shunt can have to end status testing
+ *
+ */
 // clang-format off
 //                    SSRSwitcherId SignalIdSwitch  ActiveTime  HoldTime    Shunt       CompareType MinMaxOff   ReleaseOff  MinMaxOn    ReleaseOn
 #define SSRSwitcherSwitchList \
