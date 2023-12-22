f23fbbfd1d1056887bc721537d2a8f909f81b542
DDilocker <84706575+DDilocker@users.noreply.github.com>
2023-12-22 16:21:39 +0100

    Update MappingList.h

--- lib/IROCK_212_V1_11/MappingList.h
+++ lib/IROCK_212_V1_11/MappingList.h
@@ -26,7 +26,7 @@
     MapPin(0,   AD_Shunt,           PinV_GND) \
     MapPin(0,   AD_VGnd,            PinV_GND) \
     MapPin(0,   AD_Shunt_FB,        PinFBShunt) \
-    MapPin(0,   AD_Shunt_FB,        AVREF) \
+    MapPin(0,   AD_Shunt_FB,        PinV_GND) \
     MapPin(0,   BU_C1,              PinBU_C1) \
     MapPin(0,   BU_C2,              PinBU_C2) \
     MapPin(0,   BU_C3,              PinBU_C3) \
