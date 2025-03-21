5e41afffff4856bfff03a943a21a531512aea129
DDilocker <84706575+DDilocker@users.noreply.github.com>
2025-03-21 15:22:59 +0100

    Update Pins V2.4

--- lib/IROCK_212_V2_4/PinList.h
+++ lib/IROCK_212_V2_4/PinList.h
@@ -43,25 +43,27 @@
 #define PinList \
     Pin(PinDip1,            PA15,       DigitalInputPullUpPin,      1.0,    0.0,    true,   false,  "Dip switch 1") \
     Pin(PinDip2,            PC10,       DigitalInputPullUpPin,      1.0,    0.0,    true,   false,  "Dip switch 2") \
-    Pin(PinDip3,            PC10,       DigitalInputPullUpPin,      1.0,    0.0,    true,   false,  "Dip switch 3") \
-    Pin(PinAD_C1,           PA1,        AnalogInputPin,             2.0,    0.0,    false,  false,  "Cell 5 voltage") \
-    Pin(PinAD_C2,           PA0,        AnalogInputPin,             2.0,    0.0,    false,  false,  "Cell 6 voltage") \
-    Pin(PinAD_C3,           PC3,        AnalogInputPin,             2.0,    0.0,    false,  false,  "Cell 7 voltage") \
-    Pin(PinAD_C4,           PC2,        AnalogInputPin,             2.0,    0.0,    false,  false,  "Cell 8 voltage") \
-    Pin(PinGPO_LVP,         PA8,        DigitalOutputPin,           1.0,    0.0,    true,   false,  "GPO LVP") \
-    Pin(PinGPO_OVP,         PC7,        DigitalOutputPin,           1.0,    0.0,    true,   false,  "GPO UVP") \
-    Pin(PinGPO_Alarm,       PC8,        DigitalOutputPin,           1.0,    0.0,    true,   false,  "GPO Alarm") \
-    Pin(PinBU_C1,           PB15,       DigitalOutputPin,           1.0,    0.0,    false,  false,  "BU Cell 5") \
-    Pin(PinBU_C2,           PB2,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "BU Cell 6") \
-    Pin(PinBU_C3,           PH0,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "BU Cell 7") \
-    Pin(PinBU_C4,           PC15,       DigitalOutputPin,           1.0,    0.0,    false,  false,  "BU Cell 8") \
-    Pin(PinLED1,            PB3,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "LED 1") \
-    Pin(PinLED2,            PB4,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "LED 2") \
-    Pin(PinLED3,            PB5,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "LED 3") \
+    Pin(PinDip3,            PC11,       DigitalInputPullUpPin,      1.0,    0.0,    true,   false,  "Dip switch 3") \
+    Pin(PinDip4,            PC12,       DigitalInputPullUpPin,      1.0,    0.0,    true,   false,  "Dip switch 4") \
+    Pin(PinAD_C1,           PC5,        AnalogInputPin,             2.0,    0.0,    false,  false,  "Cell 1 voltage") \
+    Pin(PinAD_C2,           PC4,        AnalogInputPin,             2.0,    0.0,    false,  false,  "Cell 2 voltage") \
+    Pin(PinAD_C3,           PA7,        AnalogInputPin,             2.0,    0.0,    false,  false,  "Cell 3 voltage") \
+    Pin(PinAD_C4,           PA6,        AnalogInputPin,             2.0,    0.0,    false,  false,  "Cell 4 voltage") \
+    Pin(PinGPO_LVP,         PH1,        DigitalOutputPin,           1.0,    0.0,    true,   false,  "GPO LVP") \
+    Pin(PinGPO_OVP,         PH0,        DigitalOutputPin,           1.0,    0.0,    true,   false,  "GPO UVP") \
+    Pin(PinGPO_Alarm,       PC15,       DigitalOutputPin,           1.0,    0.0,    true,   false,  "GPO Alarm") \
+    Pin(PinBU_C1,           PB15,       DigitalOutputPin,           1.0,    0.0,    false,  false,  "BU Cell 1") \
+    Pin(PinBU_C2,           PB2,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "BU Cell 2") \
+    Pin(PinBU_C3,           PA4,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "BU Cell 3") \
+    Pin(PinBU_C4,           PA3,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "BU Cell 4") \
+    Pin(PinLED1,            PD2,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "LED 1") \
+    Pin(PinLED2,            PB3,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "LED 2") \
+    Pin(PinLED3,            PB4,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "LED 3") \
+    Pin(PinSSR_Shunt,       PB1,        AnalogInputPin,             1.0,    0.0,    false,  false,  "SSR-Shunt") \
     Pin(PinShunt,           PC0,        AnalogInputPin,             1.0,    0.0,    false,  false,  "Shunt") \
-    Pin(PinFBShunt,         PA2,        AnalogInputPin,             1.0,    0.0,    false,  false,  "Feedback Shunt") \
+    Pin(PinFBShunt,         PA5,        AnalogInputPin,             1.0,    0.0,    false,  false,  "Feedback Shunt") \
     Pin(PinV_GND,           PC1,        AnalogInputPin,             1.0,    0.0,    false,  false,  "Virtual Ground") \
-    Pin(PinGate,            PC13,       DigitalOutputPin,           1.0,    0.0,    false,  false,  "Gate Driver") \
-    Pin(PinGatePSU,         PC14,       DigitalOutputPin,           1.0,    0.0,    false,  false,  "Gate PSU Driver") \
+    Pin(PinGate,            PC6,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "Gate Driver") \
+    Pin(PinGatePSU,         PC7,        DigitalOutputPin,           1.0,    0.0,    false,  false,  "Gate PSU Driver") \
     Pin(PinVE_Current,      NoPin,      VEDirectPin,                1.0,    0.0,    false,  false,  "Current from VE Shunt")
 // clang-format on
